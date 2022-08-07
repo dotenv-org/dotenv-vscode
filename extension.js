de6e86a8469bc2222b463d39f305ba3f730cb202
Scott Motte <mot@mot.la>
2022-08-07 12:41:00 -0700

    Add logo

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
+const DOTENV_VAULT_VERSION = '1.11.1'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
