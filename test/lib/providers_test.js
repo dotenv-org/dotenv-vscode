cedc493ce803f0b02e409f33647e6c1d8693fd67
Scott Motte <mot@mot.la>
2022-08-08 23:00:06 -0700

    wip

---
+++ test/lib/providers_test.js
@@ -0,0 +1,21 @@
+// const mocha = require('mocha')
+// const describe = mocha.describe
+// const it = mocha.it
+// const assert = require('assert')
+// const path = require('path')
+//
+// const helpers = require('../../lib/helpers')
+//
+// const doc = {
+//   fileName: 'index.js'
+// }
+//
+// console.log(path.basename(__dirname))
+//
+// describe('helpers', function () {
+//   describe('#getParsed', function () {
+//     it('parsed .env file and returns it', function () {
+//       assert.equal(helpers.getParsed(doc), {})
+//     })
+//   })
+// })
