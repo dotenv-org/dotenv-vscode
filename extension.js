2f081f071c714e7ce2a4db470ba4f12d5adaeb28
Scott Motte <mot@mot.la>
2022-09-24 05:43:10 -0700

    Patches

--- extension.js
+++ extension.js
@@ -2,14 +2,36 @@ const autocloaking = require('./lib/autocloaking')
 const autocompletion = require('./lib/autocompletion')
 const commands = require('./lib/commands')
 const peeking = require('./lib/peeking')
+const vscode = require('vscode')
 
 function activate (context) {
   console.log('Dotenv is active')
 
-  autocloaking.run(context) // turn on auto-cloaking
-  autocompletion.run(context) // autocompletion
-  commands.run(context) // vault commands
-  peeking.run(context) // secret peeking
+  // const section = 'editor.tokenColorCustomizations'
+  // // const config = vscode.workspace.getConfiguration(section).get('textMateRules')
+  // const config = vscode.workspace.getConfiguration().inspect(section).globalValue
+  // if (!config) {
+  //   console.log('Load editor.tokenColorCustomizations')
+
+  //   let value = {}
+  //   vscode.workspace.getConfiguration().update(section, value, vscode.ConfigurationTarget.Global)
+  //   value = {
+  //     'textMateRules': []
+  //   }
+  //   vscode.workspace.getConfiguration().update(section, value, vscode.ConfigurationTarget.Global)
+  // }
+
+  console.log('Load autocompletion')
+  autocompletion.run(context)
+
+  console.log('Load commands')
+  commands.run(context)
+
+  console.log('Load secret peeking')
+  peeking.run(context)
+
+  console.log('Load autocloaking')
+  autocloaking.run(context)
 }
 
 function deactivate () {
