e5691feb1bef6e57c1fea834495a5892541bc06f
Scott Motte <mot@mot.la>
2022-09-24 05:46:53 -0700

    Clean up cruft

--- extension.js
+++ extension.js
@@ -2,24 +2,9 @@ const autocloaking = require('./lib/autocloaking')
 const autocompletion = require('./lib/autocompletion')
 const commands = require('./lib/commands')
 const peeking = require('./lib/peeking')
-const vscode = require('vscode')
 
 function activate (context) {
   console.log('Dotenv is active')
-
-  // const section = 'editor.tokenColorCustomizations'
-  // // const config = vscode.workspace.getConfiguration(section).get('textMateRules')
-  // const config = vscode.workspace.getConfiguration().inspect(section).globalValue
-  // if (!config) {
-  //   console.log('Load editor.tokenColorCustomizations')
-
-  //   let value = {}
-  //   vscode.workspace.getConfiguration().update(section, value, vscode.ConfigurationTarget.Global)
-  //   value = {
-  //     'textMateRules': []
-  //   }
-  //   vscode.workspace.getConfiguration().update(section, value, vscode.ConfigurationTarget.Global)
-  // }
 
   console.log('Load autocompletion')
   autocompletion.run(context)
