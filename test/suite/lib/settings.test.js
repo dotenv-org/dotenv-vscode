60ddd48c84df934cfb8c12a709d15605abf1fd52
Scott Motte <mot@mot.la>
2023-06-14 15:39:04 -0700

    Add tests against autocloaking settings

--- test/suite/lib/settings.test.js
+++ test/suite/lib/settings.test.js
@@ -6,23 +6,29 @@ const assert = require('assert')
 const settings = require('../../../lib/settings')
 
 describe('settings', function () {
-  describe('#config', function () {
+  describe('#workspaceConfig', function () {
     it('returns workspace configuration', function () {
-      const result = settings.config()
+      const result = settings.workspaceConfig()
 
       assert.strictEqual(typeof result, 'object')
     })
   })
 
-  describe('#autocloakingOn', function () {
+  describe('#autocloakingEnabled', function () {
     it('is true by default', function () {
-      const result = settings.autocloakingOn()
+      const result = settings.autocloakingEnabled()
 
       assert.equal(result, true)
     })
 
-    it('can be turned off', function () {
+    it('can be turned on and off', async function () {
+      await settings.autocloakingOff()
+      let result = settings.autocloakingEnabled()
+      assert.equal(result, false)
 
+      await settings.autocloakingOn()
+      result = settings.autocloakingEnabled()
+      assert.equal(result, true)
     })
   })
 })
